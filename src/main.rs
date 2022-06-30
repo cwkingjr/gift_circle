db621752e8373486fab29b75becebcbf19462c86
Chuck King <cwkingjr@gmail.com>
2022-06-30 16:55:40 -0500

    repo wide replacement of pii

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 
     let person1= Person::new("Father".to_string(),"father@example.com".to_string(),1,"reading".to_string());
     let person2= Person::new("Mother".to_string(),"mother@example.com".to_string(),1,"coloring".to_string());
-    let person3= Person::new("Son 2".to_string(),"son@example.com".to_string(),2,"programming".to_string());
+    let person3= Person::new("Son".to_string(),"son@example.com".to_string(),2,"programming".to_string());
     let person4= Person::new("Daughter 2".to_string(),"duaghter2@example.com".to_string(),2,"camping".to_string());
     let person5= Person::new("Daughter".to_string(),"daughter@example.com".to_string(),3,"writing".to_string());
     let person6= Person::new("Son 2".to_string(),"son2@example.com".to_string(),3,"doctoring".to_string());
