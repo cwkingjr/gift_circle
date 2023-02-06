f79cccb2ec2e943cad5e8c1639d2d51f866c19ed
Chuck King <cwkingjr@gmail.com>
2023-02-06 14:14:38 -0600

    testing github action

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ use myargs::get_args;
 use person::Person;
 
 fn run() -> Result<()> {
+    // adding this comment to test github action
+
     let args = get_args();
 
     let mut rdr = csv::Reader::from_path(args.input.clone())
