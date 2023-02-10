f23e53354596261de86291147dca9cadbfafd3d8
Chuck King <cwkingjr@gmail.com>
2023-02-09 20:12:25 -0600

    added ability to not use groups

---
+++ develop.md
@@ -0,0 +1,27 @@
+# Develop
+
+This code was written and compiled on an Intel-based MacBook Pro (Ventura 13.0.1). If you have an Intel MacBook, you should be able to download the gift_cirle binary that is attached to the GitHub release, modify the permissions to make it executable (chmod +x gift_circle), and invoke it against your participants file as shown above.
+
+Note that the code is compiled to Ubuntu Linux via a GitHub action during the pull request process, but no build Linux binary is included within the release.
+
+If you have a different machine, you can install Rust for your machine, download the repo (git clone) or source code to your machine, and compile it from the repo folder: `cargo build --release`.
+
+You can also run these:
+
+```sh
+cargo test
+cargo build
+cargo run -- --help
+cargo run -- -u -i=./src/example-participants-with-groups.csv
+cargo run -- -i=./src/example-participants-without-groups.csv
+cargo run ./target/debug/gift_circle -u -i=./src/example-participants-with-groups.csv
+```
+
+Once the gift_circle binary is moved into your path (e.g., /usr/bin/gift_circle), of course you may invoke it like this:
+
+```sh
+gift_circle -h
+gift_circle -i=./my-participants.csv
+gift_circle -a -i=~./my-participants.csv
+gift_circle -a -u -i=~./my-participants.csv
+```
--- src/person.rs
+++ src/person.rs
@@ -3,7 +3,66 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, PartialEq, Eq, Debug, Default, Deserialize, Serialize)]
 pub struct Person {
     pub name: String,
-    pub email_address: String,
-    pub group_number: u16,
+    pub email_address: Option<String>,
+    pub group_number: Option<u16>,
     pub assigned_person_name: Option<String>,
+}
+
+impl Person {
+    #[allow(dead_code)]
+    pub fn new(name: &str, group_number: u16) -> Self {
+        Person {
+            name: name.to_string(),
+            group_number: Some(group_number),
+            ..Default::default()
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn new_no_group(name: &str) -> Self {
+        Person {
+            name: name.to_string(),
+            ..Default::default()
+        }
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, Debug, Default, Deserialize, Serialize)]
+pub struct PersonWithoutGroup {
+    pub name: String,
+    pub email_address: Option<String>,
+    pub assigned_person_name: Option<String>,
+}
+
+impl PersonWithoutGroup {
+    #[allow(dead_code)]
+    pub fn new(name: &str) -> Self {
+        PersonWithoutGroup {
+            name: name.to_string(),
+            ..Default::default()
+        }
+    }
+}
+
+impl From<Person> for PersonWithoutGroup {
+    fn from(person: Person) -> Self {
+        PersonWithoutGroup {
+            name: person.name,
+            email_address: person.email_address,
+            assigned_person_name: person.assigned_person_name,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_person_without_group_from_person() {
+        let person = Person::new("Father", 1);
+        let person_without_group = PersonWithoutGroup::from(person);
+        let expected = PersonWithoutGroup::new("Father");
+        assert_eq!(person_without_group, expected);
+    }
 }
